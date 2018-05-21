9c7199b5cbcd51bea7a02234d9aea372edc348c0
Fan Hongtao <fanhongtao@gmail.com>
2018-05-21 17:15:33 +0800

    Add empty 'Setting Activity'

--- app/src/main/java/com/fht/simpletimer/MainActivity.java
+++ app/src/main/java/com/fht/simpletimer/MainActivity.java
@@ -1,6 +1,7 @@
 package com.fht.simpletimer;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -57,12 +58,18 @@ public class MainActivity extends AppCompatActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (id) {
+            case R.id.action_settings:
+                startSettingActivity();
+                return true;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    void startSettingActivity() {
+        Intent intent = new Intent(this, SettingActivity.class);
+        startActivity(intent);
     }
 
     private void showRemainedTime(TextView textView, long remainedTime) {
