e2770fd7950b9c9fc636ec24b47cfd992438b38e
Fan Hongtao <fanhongtao@gmail.com>
2018-05-22 11:59:06 +0800

    Update SettingActivity

--- app/src/main/java/com/fht/simpletimer/SettingActivity.java
+++ app/src/main/java/com/fht/simpletimer/SettingActivity.java
@@ -1,13 +1,39 @@
 package com.fht.simpletimer;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.PreferenceFragment;
+import android.support.v7.app.AppCompatActivity;
 
 public class SettingActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_setting);
+        getFragmentManager().beginTransaction()
+                .replace(android.R.id.content, new SettingFragment())
+                .commit();
+        setTitle(R.string.title_settings);
+    }
+
+    public static class SettingFragment extends PreferenceFragment {
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.preference);
+
+            Preference prefHome = findPreference("pref_home");
+            prefHome.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+                @Override
+                public boolean onPreferenceClick(Preference preference) {
+                    Uri uri = Uri.parse("https://github.com/fanhongtao/SimpleTimer");
+                    Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                    startActivity(intent);
+                    return true;
+                }
+            });
+        }
     }
 }
